cb265fe593de200fc11e943fc2510af20128ef9c
Frederic Lassel <frederic.lassel@live.at>
2017-06-14 08:11:17 +0200

    routing und die einzelnen seiten erstellt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RootComponent } from './components/root.component';
+import { LearnComponent } from './components/learn.component';
+import { PracticeComponent } from './components/practice.component';
+
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    LearnComponent,
+    PracticeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
